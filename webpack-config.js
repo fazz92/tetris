06ab67f97e2a61bca46273b9efc253be65ccf549
Syed Mohammed Fazal <mohammedfazal94@yahoo.com>
2020-04-25 18:09:55 +0530

    Add files via upload

---
+++ webpack-config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      } 
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  }
+}
